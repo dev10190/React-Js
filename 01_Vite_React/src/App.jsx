a1f3769879497816c45435bff31bbdbd835bb8ae
Devendra <iforgetmyidok@gmail.com>
2024-09-29 18:35:07 +0530

    01_Vite_React

---
+++ 01_Vite_React/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from './chai'
+
+function App() {
+  
+
+  return (
+   <>
+   <Chai />
+   <h2>React aur Code</h2>
+   </>
+  )
+}
+
+export default App
---
+++ 01_Vite_React/src/chai.jsx
@@ -0,0 +1,9 @@
+function Chai(){
+    return(
+        <>
+            <h4>Hello, I am Chai</h4>
+        </>
+    )
+}
+
+export default Chai
