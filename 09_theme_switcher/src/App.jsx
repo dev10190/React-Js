9e95f6a60e5d810f7b19b54357c8befe20e69bfb
Devendra <iforgetmyidok@gmail.com>
2024-10-04 20:38:55 +0530

    donw with context and local storage

--- 09_theme_switcher/src/App.jsx
+++ 09_theme_switcher/src/App.jsx
@@ -7,28 +7,28 @@ import ThemeButton from "./componenets/ThemeButton";
 import Card from "./componenets/Card";
 
 function App() {
-  const [themeMode, setThemeMode] = useState('light')
+  const [themeMode, setThemeMode] = useState("light");
   const lightTheme = () => {
-    setThemeMode('light')
-  }
+    setThemeMode("light");
+  };
 
   const darkTheme = () => {
-    setThemeMode('dark')
-  }
+    setThemeMode("dark");
+  };
 
   // actual theme in change
 
   useEffect(() => {
-    document.querySelector('html').classList.remove('light', 'dark')
-    document.querySelector('html').classList.add(themeMode)
-  }, [themeMode])
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <ThemeProvider value={{themeMode, lightTheme, darkTheme }}>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-           <ThemeButton />
+            <ThemeButton />
           </div>
 
           <div className="w-full max-w-sm mx-auto">
